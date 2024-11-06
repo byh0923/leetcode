e98fac593e9855f7e324405a0f6a8d702eb7fc05
배영현 <byh2949@gmail.com>
2024-11-06 19:33:11 +0900

    leetcode Continas Duplicate 문제
    list의 contains를 사용하면 배열을 또 탐색한다..  set 사용

---
+++ src/main/java/com/array/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package com.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if(!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate solution = new ContainsDuplicate();
+        System.out.println(solution.containsDuplicate(new int[] {1,2,3,1}));
+    }
+}
